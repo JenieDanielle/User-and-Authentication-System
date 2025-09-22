20f1924b32ba4bf54af37f49c90257d3023112ca
Jenie <jdft1101@gmail.com>
2025-09-22 17:15:47 -0300

    Realização de testes

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require_once "AuthenticateUser.php";
+
+$usuario = new AuthenticateUser();
+
+echo $usuario->newUser("Maria Oliveira", "maria@email.com", "Senha123") . PHP_EOL;
+echo $usuario->newUser("Pedro", " pedro@@email", "Senha123") . PHP_EOL;
+echo $usuario->newUser("Fernando", "maria@email.com", "Senha123") . PHP_EOL;
